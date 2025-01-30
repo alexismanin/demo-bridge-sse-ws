8f2dcb80723d354ffe217623e471c05893a5b85d
Alexis Manin <alexis.manin@gmail.com>
2025-01-30 06:41:09 +0100

    Initialize Spring Boot app

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  id("org.springframework.boot") version "3.4.2"
+  id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "fr.amanin.demo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+  implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.12.0")
+}
+
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
+}
+
+springBoot {
+  mainClass = "fr.amanin.demo.DemoAppKt"
+}
